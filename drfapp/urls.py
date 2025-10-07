2bfefd5184ff684358f035f19358484f444dfd06
lDarkManl <81003396+lDarkManl@users.noreply.github.com>
2025-10-07 16:01:51 +0700

    frontend

--- drfapp/urls.py
+++ drfapp/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import GetCapitalInfoView
+from .views import GetCapitalInfoView, AppView, AppListView, AppCreateView
 
 
 urlpatterns = [
     path('api/capitals/', GetCapitalInfoView.as_view()),
+    path('app/', AppView.as_view(), name='app'),
+    path('app/list/', AppListView.as_view(), name='app-list'),
+    path('app/create/', AppCreateView.as_view(), name='app-create'),
 ]
+
 
 
--- drfapp/views.py
+++ drfapp/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.views.generic import TemplateView
 
 from .models import Capital
 from .serializers import CapitalSerializer
@@ -19,3 +20,15 @@ class GetCapitalInfoView(APIView):
         serializer_for_writing.is_valid(raise_exception=True)
         serializer_for_writing.save()
         return Response(serializer_for_writing.data, status=status.HTTP_201_CREATED)
+
+
+class AppView(TemplateView):
+    template_name = 'drfapp/index.html'
+
+
+class AppListView(TemplateView):
+    template_name = 'drfapp/list.html'
+
+
+class AppCreateView(TemplateView):
+    template_name = 'drfapp/create.html'
